8c59d91658728b4680cdc3d480b132957c137f10
vikyij <vikkyij54@gmail.com>
2022-05-16 16:13:01 +0100

    write test for successful api call on page load

--- src/pages/Home/__test__/home.test.js
+++ src/pages/Home/__test__/home.test.js
@@ -2,8 +2,9 @@ import Home from '../index'
 import { render, screen } from '@testing-library/react'
 import { Provider } from 'react-redux'
 import store from '../../../redux/store'
+import userEvent from '@testing-library/user-event'
 
-const HomeProvider = ({ mode }) => {
+const ReduxProvider = ({ mode }) => {
   return (
     <Provider store={store}>
       <Home mode={mode} />
@@ -13,10 +14,27 @@ const HomeProvider = ({ mode }) => {
 
 describe('write tests for homepage', () => {
   test('display invoice on page load', async () => {
-    render(<HomeProvider mode='dark' />)
+    render(<ReduxProvider mode='dark' />)
 
     const divWrapper = await screen.findByTestId('div-wrapper-0')
     expect(divWrapper).toBeInTheDocument()
   })
 })
-afterEach(() => jest.resetAllMocks())
+
+test('invoice details page is displayed when an invoice is clicked', async () => {
+  render(<ReduxProvider mode='dark' />)
+
+  const divWrapper = await screen.findByTestId('div-wrapper-0')
+  userEvent.click(divWrapper)
+  const statusText = await screen.findByText(/grand total/i)
+  expect(statusText).toBeInTheDocument()
+})
+
+test('new invoice is displayed when new invoice button is clicked', () => {
+  render(<ReduxProvider mode='dark' />)
+
+  const newInvoiceBtn = screen.getByAltText('add-new-invoice-icon')
+  userEvent.click(newInvoiceBtn)
+  const newInvoiceText = screen.getByText('New Invoice')
+  expect(newInvoiceText).toBeInTheDocument()
+})
